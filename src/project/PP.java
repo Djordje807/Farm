1a85b67dc28986a46bc8565451f4e56d1419a491
korisnik <korisnik@Z1k4>
2024-12-09 17:32:57 +0100

    Dofajem pp

---
+++ src/project/PP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package project;
+
+/**
+ *
+ * @author korisnik
+ */
+public class PP {
+    int i =10;
+    int j = 5;
+    int c = i+ j;
+   
+}
